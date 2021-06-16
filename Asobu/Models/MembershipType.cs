4e2c6ef4c3d20edbd721823320d1680d4888339c
Bryan Duarte <dellinspiron15BEDL@outlook.com>
2021-06-16 11:20:36 -0500

    Added Name to Membership Types

--- Asobu/Models/MembershipType.cs
+++ Asobu/Models/MembershipType.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Asobu.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        
         public short SignUpFee { get; set; }
+        
         public byte DurationInMonths { get; set; }
+        
         public decimal DiscountRate { get; set; }
     }
 }
